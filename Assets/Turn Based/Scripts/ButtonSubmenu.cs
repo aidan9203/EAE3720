724accd87e22cd6012810b4f182f8df2840a4cec
ParimalRaghavan <42382731+ParimalRaghavan@users.noreply.github.com>
2020-04-01 23:40:55 -0600

    Turn Based changes

---
+++ Assets/Turn Based/Scripts/ButtonSubmenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSubmenu : MonoBehaviour
+{
+    public Button buttonTL;
+
+    public Button buttonTR;
+
+    public Button buttonBL;
+
+    public Button buttonBR;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Turn Based/Scripts/Player.cs
+++ Assets/Turn Based/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
@@ -11,6 +12,14 @@ public class Player : MonoBehaviour
     public GameObject grenadeCountText;
 
     public GameObject healthCounter;
+
+    public Button button1;
+
+    public Button button2;
+
+    public Button button3;
+
+    public Button button4;
 
     public int initialHealth = 300;
 
@@ -28,21 +37,77 @@ public class Player : MonoBehaviour
 
     private System.Random rand = new System.Random();
 
+    private  Button[] buttonList = null;
+
+    private Button selectedButton = null;
+
+    private Button subSelectedButton = null;
+
+    private int selectedButtonIndex = -1;
+
+    private ButtonSubmenu submenu = null;
+
+    bool actionMenu = false;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = initialHealth;
         TextMeshProUGUI healthText = healthCounter.GetComponent<TextMeshProUGUI>();
         healthText.SetText(currentHealth.ToString());
+        Button[] buttonListCreator ={button1, button2, button3, button4};
+        buttonList = buttonListCreator;
+        selectedButton = button1;
+        selectedButton.Select();
+        selectedButtonIndex = 0;
+        actionMenu = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (currentHealth <= 0)
         {
             Destroy(this.gameObject);
         }
+
+        Debug.Log(EventSystem.current.currentSelectedGameObject);
+        
+        if (actionMenu)
+        {
+            if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && (selectedButtonIndex <= 2))
+            {
+                selectedButtonIndex += 1;
+                selectedButton = buttonList[selectedButtonIndex];
+                selectedButton.Select();
+            }
+
+            else if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && (selectedButtonIndex >= 1))
+            {
+                selectedButtonIndex -= 1;
+                selectedButton = buttonList[selectedButtonIndex];
+                selectedButton.Select();
+            }
+
+            else if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Z))
+            {
+                selectedButton.onClick.Invoke();
+                actionMenu = false;
+                submenu = selectedButton.GetComponent<ButtonSubmenu>();
+                subSelectedButton = submenu.buttonTL;
+                EventSystem.current.SetSelectedGameObject(null);
+                StartCoroutine(SelectButtonLater(subSelectedButton));
+            }
+        }
+
+        else
+        {
+            selectedButton.onClick.Invoke();
+        }
+
+
     }
 
     public void punchAttack()
@@ -135,5 +200,12 @@ public class Player : MonoBehaviour
     private void ActivateTurn()
     {
         playerTurn = true;
+    }
+
+    private IEnumerator SelectButtonLater(Button button)
+    {
+        yield return null;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(button.gameObject);
     }
 }
